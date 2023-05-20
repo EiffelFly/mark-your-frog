af6f623fa7473361e126a2f1e306374f7a9871f8
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-05-20 20:18:08 +0800

    feat: update prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  printWidth: 100,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: true,
+  overrides: [
+    {
+      files: [".*", "*.json", "*.md", "*.toml", "*.yml"],
+      options: {
+        useTabs: false,
+      },
+    },
+  ],
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
