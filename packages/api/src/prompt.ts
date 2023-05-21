8301a8940d2b2880d5d8cd93449f201dfec5548d
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-05-21 23:14:05 +0800

    feat: store some prompt in api

---
+++ packages/api/src/prompt.ts
@@ -0,0 +1,26 @@
+const template = `### You are a smart and powerful assistant, 
+  
+  and you need to help me find the most important task that I need to finish from the list. 
+  
+  In order to make the answer precise, let's think step by step, but you don't need to include your
+  
+  reasoning in the answer. But try to fill in the below format ###
+  
+  Desired format should be similar to this, you put the choosen todo into todo key: {{
+    todo: {{
+      "id": 6894440006,
+      "content": "hello world",
+      "priority": 1,
+      "due": {{
+        "date": "2021-10-10",
+        "string": "5月21日",
+        "lang": "tw",
+        "isRecurring": false
+      }},
+      "description": "",
+      "url": "https://todoist.com/showTask?id=6894440006"
+    }},
+    "reason": "the reason why you think this is the most important task in 100 words"
+  }}
+  
+  INPUT: {todos}`;
