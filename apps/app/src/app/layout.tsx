f2570ccf4efd6ffbbce7b692a2411eb61f5940cd
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-05-21 23:09:41 +0800

    chore: refactor the main layout and page of app

--- apps/app/src/app/layout.tsx
+++ apps/app/src/app/layout.tsx
@@ -3,28 +3,17 @@ import { NavBar } from "@/components";
 import { getServerSession } from "next-auth";
 // include styles from the ui package
 
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const session = await getServerSession();
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+	const session = await getServerSession();
 
-  return (
-    <html lang="en" className="bg-zinc-900">
-      <body>
-        <div className="flex flex-col min-h-screen">
-          <NavBar user={session?.user || null} />
-          <div className="flex flex-col flex-1 pt-20 lg:pt-[240px] max-w-[800px] mx-auto">
-            <div className="flex flex-col">
-              <h1 className="mb-20 text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl">
-                Mark your Frog
-              </h1>
-              {children}
-            </div>
-          </div>
-        </div>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className="bg-zinc-900">
+			<body>
+				<div className="flex min-h-screen flex-col">
+					<NavBar user={session?.user || null} />
+					{children}
+				</div>
+			</body>
+		</html>
+	);
 }
--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -1,10 +1,23 @@
 import { Metadata } from "next";
-import { MarkYourFrogButton } from "@/components";
+import { getServerSession } from "next-auth";
+import { MarkYourFrog } from "../components";
+import { authOptions } from "@/lib/auth";
 
 export const metadata: Metadata = {
-  title: "Web - Turborepo Example",
+	title: "Mark Your Frog",
 };
 
 export default async function Home() {
-  return <MarkYourFrogButton />;
+	const session = await getServerSession(authOptions);
+
+	return (
+		<div className="mx-auto flex max-w-[800px] flex-1 flex-col pt-20 lg:pt-[240px]">
+			<div className="flex flex-col">
+				<h1 className="mb-20 text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl">
+					Mark your Frog
+				</h1>
+				{session?.access_token ? <MarkYourFrog accessToken={session.access_token} /> : null}
+			</div>
+		</div>
+	);
 }
