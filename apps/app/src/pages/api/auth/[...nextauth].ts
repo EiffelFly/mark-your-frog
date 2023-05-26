76d9bba9cca9d35bfa11573902af02734944ff92
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-05-26 11:14:13 +0800

    fix: fix next_auth host url missing issue

--- apps/app/src/pages/api/auth/[...nextauth].ts
+++ apps/app/src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,15 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default NextAuth(authOptions);
+export default function auth(req: NextApiRequest, res: NextApiResponse) {
+	const { host } = req.headers;
+
+	if (!host) {
+		return res.status(400).send(`Bad Request, missing host header`);
+	}
+
+	process.env.NEXTAUTH_URL = /localhost/.test(host) ? `http://${host}` : host;
+
+	return NextAuth(authOptions)(req, res);
+}
