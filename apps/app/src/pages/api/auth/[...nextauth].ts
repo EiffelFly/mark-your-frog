46d71ce3e7650e4e993e05b286471c2bb0322c41
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-05-27 09:57:33 +0800

    fix: disable dynamic next-auth base url

--- apps/app/src/pages/api/auth/[...nextauth].ts
+++ apps/app/src/pages/api/auth/[...nextauth].ts
@@ -3,13 +3,12 @@ import { authOptions } from "@/lib/auth";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function auth(req: NextApiRequest, res: NextApiResponse) {
-	const { host } = req.headers;
+	// const { host } = req.headers;
 
-	if (!host) {
-		return res.status(400).send(`Bad Request, missing host header`);
-	}
+	// if (!host) {
+	// 	return res.status(400).send(`Bad Request, missing host header`);
+	// }
 
-	process.env.NEXTAUTH_URL = /localhost/.test(host) ? `http://${host}` : host;
-
+	// process.env.NEXTAUTH_URL = /localhost/.test(host) ? `http://${host}` : host;
 	return NextAuth(authOptions)(req, res);
 }
